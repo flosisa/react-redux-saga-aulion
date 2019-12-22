e23dec65367b5a41957cd4cd7704b6b1df7f00ac
Laziz <flosisa>
2019-12-22 12:15:19 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
-import Index from 'Components/index.js';
+import Index from 'Components';
 import { history } from './';
 
 import 'Assets/style/bootstrap.min.css';
